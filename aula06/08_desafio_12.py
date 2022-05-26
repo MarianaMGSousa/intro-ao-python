abe7afb9ea769e325d450f06743575ed551086df
MarianaMGSousa <marianamoreira2608@gmail.com>
2022-05-26 09:25:22 -0300

    desofio 12 da aula 6

--- aula06/08_desafio_12.py
+++ aula06/08_desafio_12.py
@@ -1,3 +1,20 @@
+import csv
+import json
+
 # Escreva um programa que leia o arquivo "exemplo2.csv", e converta
 # os registros desse CSV para o formato JSON. Escreva um arquivo de saída
 # que contenha o conteúdo em JSON.
+leitura_csv = 'exemplo2.csv'
+with open(leitura_csv, mode = 'r') as arquivo:
+    leitor = csv.DictReader(arquivo) #considera que a primeira linha é cabeçalho
+    conteudo = []
+    for registro in leitor:  #convertendo para dicionário
+        conteudo.append(registro)
+print(conteudo)
+
+conteudo_json = json.dumps(conteudo)
+print(conteudo_json)
+       
+arquivo_json = 'exemplo2_saida.json'
+with open(arquivo_json, 'w') as arquivo2:
+    arquivo2.writelines(conteudo_json)
