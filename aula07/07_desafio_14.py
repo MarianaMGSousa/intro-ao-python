8886e7ad7d4fb260333ccdd041f99977ed00042d
MarianaMGSousa <marianamoreira2608@gmail.com>
2022-05-29 17:53:05 -0300

    resolução do desafio 14

--- aula07/07_desafio_14.py
+++ aula07/07_desafio_14.py
@@ -11,13 +11,40 @@ import json
 # Utilize uma biblioteca requests para ler o JSON.
 # Ao final do programa, imprima os dados sobre o feitiço que ele solicita.
 # Se o feitiço não foi encontrado, lance uma exceção.
-URL = "https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json"
-resposta = requests.get(URL)
-conteudo = resposta.text
-print('Código da resposta:', resposta.status_code)
-print('Tipo da resposta:', resposta.headers['Content-Type']) # text/html
-print('Conteúdo:')
-print(conteudo)
-print('--------------------------------------------------')
-print()
 
+def solicitar_feitico():
+    feitico = input('Digite o nome do feitiço: ')
+    feitico = feitico.lower()
+    return feitico
+
+def arquivo_json():
+    URL = "https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json"
+    resposta = requests.get(URL)
+    dicionario = json.loads(resposta.content.lower())
+    print('Código da resposta:', resposta.status_code)
+    print('Tipo da resposta:', resposta.headers['Content-Type'])
+    print()
+    return dicionario
+
+
+
+def consulta_feitico():
+    feitico = solicitar_feitico() 
+    dicionario = arquivo_json() 
+
+    try:
+        if dicionario[feitico]:
+            print('Feitiço encontrado!!!\n')
+            print(f'Feitiço: ', feitico.upper())
+            for chave, valor in dicionario[feitico].items():
+                resultado = (f'{chave}: {valor}')
+                print(resultado.title())
+            print('\n')
+            
+    except KeyError:
+        print('\n')
+        print(" ---  FEITIÇO NÃO ENCONTRADO!  ---")
+        print('\n')
+ 
+ 
+consulta_feitico()       
